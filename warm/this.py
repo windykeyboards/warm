1dc9dd230aa0a69324f4cbe1c39a6e01358546f6
Jamie Sanson <jamie.sanson@trademe.co.nz>
2018-06-22 19:03:38 +1200

    Adds TODOs

--- warm/this.py
+++ warm/this.py
@@ -5,3 +5,10 @@ class This(Action):
 
     def run(self):
         print "This run"
+
+    def __find_src_dir(self):
+        # TODO - Find lowest level directory containing .cpp or .h files
+        return ""
+
+    def __generate_warm_file(self, name, src_dir):
+        pass
--- warm/up.py
+++ warm/up.py
@@ -5,3 +5,19 @@ class Up(Action):
 
     def run(self):
         print "Up run"
+
+    def __diff_to_current(self, dependencies):
+        # TODO - Return list of dependencies which need to be fetched
+        return []
+
+    def __parse_dependencies(self):
+        # TODO - Read and construct data classes defining dependencies
+        return []
+
+    def __download_and_apply(self, dependency):
+        # TODO - Download given dependency, parse warm file and move files to the right place
+        return
+
+    def __output_results(self, depresults):
+        print 'Done'
+    
